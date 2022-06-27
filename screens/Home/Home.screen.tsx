faf567e594dd9a5b39f33b6c7387c91a5b5089b9
maxim <fedaraumaxim@gmail.com>
2022-06-27 12:05:43 +0300

    Installed React Navigator, React Navigator Stack and set up them

---
+++ screens/Home/Home.screen.tsx
@@ -0,0 +1,11 @@
+//Types
+import { ReactElement } from "react";
+
+//React Native
+import { View, Text } from "react-native";
+
+export default function Home(): ReactElement {
+  return <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <Text>Homee</Text>
+  </View>;
+}
